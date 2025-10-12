0fcb05ff2ed0fa2b0db959e197be5cec8260a434
Jairik <jairikmc@gmail.com>
2025-10-12 02:54:29 -0400

    Adding user fetch queries
    Adding some queries and whatnot, need to update the getPatientHistory call to reflect more recently database changes.

--- backend/queries/general_queries.py
+++ backend/queries/general_queries.py
@@ -24,5 +24,59 @@ def fetchAllClients():
     response = run_query(sql)
     return response.get('records', [])
 
-r = fetchAllClients()
-print("All records:", r)
+def validateClientExists(firstName: str, lastName: str, dob: Date):
+    ''' Check if a patient exists in the DB for further logic checks '''
+    sql = """
+    SELECT COUNT(*) FROM client
+    WHERE client_fn = :firstName AND client_ln = :lastName AND client_dob = :dob;
+    """
+    params = [
+        {'name': 'firstName', 'value': {'stringValue': firstName}},
+        {'name': 'lastName', 'value': {'stringValue': lastName}},
+        {'name': 'dob', 'value': {'stringValue': dob.isoformat()}}
+    ]
+    response = run_query(sql, params)
+    count = int(response['records'][0][0]['longValue'])
+    return count > 0
+
+def addUserInfo(firstName: str, lastName: str, dob: Date, insPolId: int):
+    ''' Add a new patient to the DB '''
+    sql = """
+    INSERT INTO client (client_fn, client_ln, client_dob, ins_pol_id)
+    VALUES (:firstName, :lastName, :dob, :insPolId);
+    """
+    params = [
+        {'name': 'firstName', 'value': {'stringValue': firstName}},
+        {'name': 'lastName', 'value': {'stringValue': lastName}},
+        {'name': 'dob', 'value': {'stringValue': dob.isoformat()}},
+        {'name': 'insPolId', 'value': {'longValue': insPolId}}
+    ]
+    run_query(sql, params)
+    return {"success": True}
+
+# TODO TODO TODO - FIX ONCE DB IS UPDATED - TODO TODO TODO
+def getPatientHistory(firstName: str, lastName: str, dob: Date):
+    ''' Retrieve patient history from the DB '''
+    sql = """
+    SELECT * FROM client
+    WHERE client_fn = :firstName AND client_ln = :lastName AND client_dob = :dob;
+    """
+    params = [
+        {'name': 'firstName', 'value': {'stringValue': firstName}},
+        {'name': 'lastName', 'value': {'stringValue': lastName}},
+        {'name': 'dob', 'value': {'stringValue': dob.isoformat()}}
+    ]
+    response = run_query(sql, params)
+    records = response.get('records', [])
+    
+    # Validate that records were found
+    if not records:
+        return "No history found."
+    
+    # Format the records into a readable string
+    history = []
+    for record in records:
+        formatted_record = ". ".join(f"{key}: {value.get(list(value.keys())[0])}" for key, value in zip(['client_id', 'client_fn', 'client_ln', 'client_dob', 'ins_pol_id'], record))
+        history.append(formatted_record)
+    
+    return "\n".join(history)
